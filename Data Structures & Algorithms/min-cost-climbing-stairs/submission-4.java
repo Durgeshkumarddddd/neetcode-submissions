3591174a9325ec2a3f44a1ec57bae4c4691052e1
Durgesh Kumar <145250270+Durgeshkumarddddd@users.noreply.github.com>
2026-06-05 23:27:56 +0530

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.java
@@ -0,0 +1,65 @@
+
+class Solution {
+  /*  // Using Recursion
+    public static int solve(int[] cost, int n){
+      if (n == 0 || n == 1) 
+        return 0;
+
+      // Jump from onestep before
+        int prev1 = solve(cost, n - 1) + cost[n - 1];
+      // Jump from two step before
+        int prev2 = solve(cost, n - 2) + cost[n-2];
+        return Math.min(prev1, prev2);  
+    }
+
+    */
+
+  /*  // Using Memoization
+    public static int solve(int[] cost, int n, int[] dp){
+      if(n == 0 || n == 1)
+        return 0;
+
+      if (dp[n] != -1)
+        return dp[n];
+
+      return dp[n] = Math.min(solve(cost, n -1,dp) + cost[n-1], solve(cost, n - 2,dp) + cost[n-2]);  
+
+    }
+  
+  */
+    
+    // Using Tabulation Method
+    public static int solve(int[] cost, int n){
+    int dp[] = new int[n + 2];
+    dp[n] = 0 ;
+    dp[n + 1] = 0;
+
+
+    for ( int i = n - 1 ; i >= 0 ; i-- ){
+      dp[i] = cost[i] + Math.min (dp[i + 1] , dp[i + 2]) ;
+    }
+    return Math.min(dp[0], dp[1]) ;
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+      // int prev2 = cost[0];
+      //   int prev1 = cost[1];
+
+      //   for(int i = 2; i < cost.length; i++) {
+
+      //       int curr = cost[i]
+      //           + Math.min(prev1, prev2);
+
+      //       prev2 = prev1;
+      //       prev1 = curr;
+      //   }
+
+      //   return Math.min(prev1, prev2);
+
+        int n = cost.length ;
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+        return solve(cost, n);
+
+    }
+}
